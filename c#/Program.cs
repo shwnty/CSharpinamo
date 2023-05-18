ba3f4891d838524052607fd8cf826bd7c0f77095
Shawn <senseishawnplays@gmail.com>
2023-05-19 01:15:59 +0800

    add string lesson

---
+++ c#/Program.cs
@@ -0,0 +1,27 @@
+﻿// program 1
+Console.WriteLine("Name: Shawn Tyrone D. Rada");
+
+//program 2
+Console.WriteLine("Name: " + "Shawn Tyrone D. Rada");
+
+//program 3
+string name = "Shawn Tyrone D. Rada";
+Console.WriteLine("Name: " + name);
+
+//program 4
+Console.WriteLine($"Name: {name}");
+
+//program 5
+
+string nameko = "Shawn";
+string namegf = "Mary";
+
+Console.WriteLine($"Ako si {nameko} at miss ko na si {namegf}.");
+
+//program 6
+Console.WriteLine($"The Name {nameko} has {nameko.Length} Letters");
+Console.WriteLine($"The Name {namegf} has {namegf.Length} Letters");
+
+
+
+
