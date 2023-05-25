03814016bc2cf3bd65226ebb7819ea3af238c119
Shawn <senseishawnplays@gmail.com>
2023-05-25 18:03:15 +0800

    difference or && and || operations

---
+++ csBranches/c#branches/Program.cs
@@ -0,0 +1,31 @@
+﻿// difference of and or operations
+
+// and operations
+int a = 6;
+int b = 6;
+int c = 12;
+
+if((a + b > 10) && (a == b)){
+    Console.WriteLine("The Sum is Greater than 12 and a and b are equal to c");
+}else{
+     Console.WriteLine("ulol hahahah mali");
+     }
+
+if((a + b < 10) && (a == b)){
+    Console.WriteLine("The Sum is Greater than 12 and a and b are equal to c");
+}else{
+     Console.WriteLine("ulol hahahah mali");
+     }
+
+//or operations
+if((a + b > 10) || (a + b == c)){
+    Console.WriteLine("The Sum is Greater than 12 and a and b are equal to c");
+}else{
+     Console.WriteLine("ulol hahahah mali");
+     }
+
+if((a + b < 10) || (a + b == c)){
+    Console.WriteLine("The Sum is Greater than 12 and a and b are equal to c");
+}else{
+     Console.WriteLine("ulol hahahah mali");
+     }
